d4ff6eb208f9964bd67378b74d0d0d9e932b37b5
nmsn <136696700@qq.com>
2023-02-21 21:42:42 +0800

    refactor: remove chore

--- packages/components/src/Detail/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Descriptions } from "./Description";
-export { default as Title } from "./Title";
-export { default as LineableText } from "./LineableText";
-export { default as Table } from "./Table";
