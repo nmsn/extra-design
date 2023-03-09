a3a0e7acfa4a2c5998fd908594f8aeac8bd04345
nmsn <136696700@qq.com>
2023-03-09 20:24:29 +0800

    feat: update  rollup

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -2,16 +2,16 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import json from "@rollup/plugin-json";
 import babel from "@rollup/plugin-babel";
-
+import postcss from "rollup-plugin-postcss";
+import less from "less";
 // 入口文件
-const entry = "src/index.ts";
+const entry = "src/index.tsx";
 
 // babel配置
 const babelOptions = {
   presets: ["@babel/preset-env"],
-  extensions: [".js", ".jsx", ".ts", ".tsx", ".scss"],
+  extensions: [".js", ".jsx", ".ts", ".tsx"],
   exclude: "**/node_modules/**",
 };
 
@@ -20,17 +20,29 @@ export default {
   // 入口
   input: entry,
   // 打包信息
-  output: [{ filname: "index.esm.js", dir: "dist/es/", format: "esm" }],
+  output: [{ filename: "index.esm.js", dir: "dist/es/", format: "esm" }],
   // 插件配置
   plugins: [
-    // 可使用 `import {module} from './file'` 替换 `import {module} from './file/index.js`
-    resolve(),
+   
+    postcss({
+      // minimize: true,
+      // modules: true,
+      use: {
+        sass: null,
+        stylus: null,
+        less: { javascriptEnabled: true },
+      },
+      extract: true,
+    }),
+     // 可使用 `import {module} from './file'` 替换 `import {module} from './file/index.js`
+     resolve(),
+
+    
     // 支持commonjs，包括第三方引入使用到commonjs语法
     commonjs(),
     // typescript支持
     typescript(),
-    // 支持读取json文件
-    json(),
+
     // babel
     babel(babelOptions),
   ],
